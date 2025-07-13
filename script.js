dd5ae56da603925c0dc51f354749a89a5447aef7
Angela <angela.xq.yang@gmail.com>
2025-07-12 22:16:31 -0700

    improved nav bar, content, and footer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//parallax scrolling
 let text = document.getElementById('text');
 let text2 = document.getElementById('text2');
 let back1 = document.getElementById('back1');
@@ -9,17 +10,61 @@ var width= Math.max(body.scrollWidth, body.offsetWidth);
 
 window.addEventListener('scroll', () => {
   const value = window.scrollY;
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
   text.style.marginTop = Math.min(value * 2.5, window.outerHeight)+ 'px';
   text2.style.marginTop = Math.min(value * 2.5, window.outerHeight) + 'px';
   back1.style.left = Math.min(value * 2.5, width) + 'px';
   back2.style.left = Math.min(value * -1.5, width) + 'px';
-  back3.style.top = Math.min(value * 2.5, height) + 'px';
+  back3.style.top = Math.min(value * 2.5, maxScroll) + 'px';
 });
 
+//studio ghibli side animations
 let back4 = document.getElementById('bg');
 let back5 = document.getElementById('bg2');
 window.addEventListener('scroll', () => {
   const value = window.scrollY;
   back5.style.top = Math.min(value * 0.5, height) + 'px';
   back4.style.top = Math.min(value * 0.5, height) + 'px';
+});
+
+//navbar shrinking animation
+window.addEventListener("scroll", () => {
+  const nav = document.querySelector(".navigation");
+  const collapsed = document.getElementById("collapsed-nav");
+
+  if (window.scrollY > 100) {
+    nav.classList.add("nav-shrunk");
+    collapsed.style.display = "block";
+  } else {
+    nav.classList.remove("nav-shrunk");
+    collapsed.style.display = "none";
+  }
+});
+
+//returns to top when shrunk navbar clicked on
+document.getElementById("collapsed-nav").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+
+//navbar hovering smooth background animation
+const hoverBg = document.querySelector('.nav-hover-bg');
+const navLinks = document.querySelectorAll('.header-link');
+const navLinksContainer = document.querySelector('.nav-links');
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseenter', () => {
+    const linkRect = link.getBoundingClientRect();
+    const containerRect = navLinksContainer.getBoundingClientRect();
+    
+    const offsetLeft = linkRect.left - containerRect.left;
+    const width = linkRect.width;
+
+    hoverBg.style.left = `${offsetLeft}px`;
+    hoverBg.style.width = `${width}px`;
+  });
+});
+
+navLinksContainer.addEventListener('mouseleave', () => {
+  hoverBg.style.width = '0px';
 });
